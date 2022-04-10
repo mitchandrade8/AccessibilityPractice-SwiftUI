9ea3340e1aca3931bc68f9f37f07027c05c68684
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-09 19:15:37 -0600

    Initial Commit

---
+++ accessibilityPractice/accessibilityPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  accessibilityPracticeApp.swift
+//  accessibilityPractice
+//
+//  Created by Mitch Andrade on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct accessibilityPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
