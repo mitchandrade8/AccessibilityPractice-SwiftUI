6a225937a28241fc8330e76a8bc0d0ff7e6ce102
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-09 20:02:44 -0600

    second commit

---
+++ accessibilityPractice/Practice/HidingAndGroupingAccessibilityData.swift
@@ -0,0 +1,20 @@
+//
+//  HidingAndGroupingAccessibilityData.swift
+//  accessibilityPractice
+//
+//  Created by Mitch Andrade on 4/9/22.
+//
+
+import SwiftUI
+
+struct HidingAndGroupingAccessibilityData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HidingAndGroupingAccessibilityData_Previews: PreviewProvider {
+    static var previews: some View {
+        HidingAndGroupingAccessibilityData()
+    }
+}
